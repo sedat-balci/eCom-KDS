55dac0f23b3da614bae19ed633b1a0842a1970d2
Sedat Balci <sedatbalci035@gmail.com>
2025-12-21 22:46:12 +0300

    nihai versiyon eklendi

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+// Bağlantı ayarları
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',      
+    password: '',      
+    database: 'eCom_dss' 
+});
+
+// Bağlantıyı aç ve terminale durumu yaz
+connection.connect((err) => {
+    if (err) {
+        console.error('❌ Veritabanı bağlantı hatası: ' + err.stack);
+        return;
+    }
+    console.log('✅ MySQL Veritabanına başarıyla bağlanıldı! ID: ' + connection.threadId);
+});
+
+module.exports = connection;
